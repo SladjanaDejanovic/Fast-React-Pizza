24f88fcd35dead00cb956e8a4ec30ab3165d680f
Sladjana <csl.bunny.2207@gmail.com>
2024-06-24 18:49:50 +0200

    flexvox

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function CartOverview() {
   return (
-    <div className="bg-stone-800 text-stone-200 uppercase  text-sm px-4 py-4 sm:px-6 md:text-base">
+    <div className="bg-stone-800 text-stone-200 text-sm px-4 py-4 sm:px-6 md:text-base flex items-center justify-between uppercase">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Sladjana</div>;
+  return <div className="hidden md:block text-sm font-semibold">Sladjana</div>;
 }
 
 export default Username;
