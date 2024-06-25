e5de4a54b1dbdef6b7e059a13575a37190d2745c
Sladjana <csl.bunny.2207@gmail.com>
2024-06-25 23:45:07 +0200

    styling menu

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500 font-pizza flex items-center justify-between uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
+    <header className="bg-yellow-500 font-pizza flex flex-col md:flex-row md:items-center md:justify-between uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
