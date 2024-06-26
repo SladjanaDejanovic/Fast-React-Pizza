702cedd691299ec374f509e2bbe9200991c3ede8
Sladjana <csl.bunny.2207@gmail.com>
2024-06-26 21:58:15 +0200

    styling cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " py-3 px-4 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "border-2 border-stone-300 uppercase tracking-wide rounded-full font-semibold text-stone-400  inline-block hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:bg-yellow-300 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5",
   };
 
   if (to)
