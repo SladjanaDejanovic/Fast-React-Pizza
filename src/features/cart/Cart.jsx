4ad360357f4eecc6094536a8f4c0ea9d9e1cd675
Sladjana <csl.bunny.2207@gmail.com>
2024-07-02 20:52:51 +0200

    updating item quantity

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -42,7 +42,7 @@ function Cart() {
       </h2>
       <ul className="divide-y divide-stone-200 border-b mt-3">
         {cart.map((item) => (
-          <CartItem item={item} key={item.id} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
       <div className="mt-6 space-x-2">
